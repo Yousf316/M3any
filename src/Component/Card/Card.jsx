eed108b69514eb7c36c85651acc6a7b1c60f4685
Yousf316 <youseframzii@outlook.com>
2024-06-15 18:07:58 +0300

    update url card

--- src/Component/Card/Card.jsx
+++ src/Component/Card/Card.jsx
@@ -14,7 +14,7 @@ function Card({Title ="اسم المانجا" , Link =""})
             <br />
             <br />
 
-            <a  href={'/name/'+Link} target="_blank" ><h1>{Title}</h1></a>
+            <a  href={'/M3any/name/'+Link} target="_blank" ><h1>{Title}</h1></a>
             
             {/* <h2 className={styles.title}> {Title}</h2> */}
            
